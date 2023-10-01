2591b5768864313eebc5b15feb2e4b2360276a16
Jordan <zhao.jordan@hotmail.com>
2023-10-01 00:34:20 -0700

    feat: Connection to API successful, testing with start_live_view but will require fix to display

--- app/app.py
+++ app/app.py
@@ -1,11 +1,60 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 import sys
 import socket
 import requests
 import xml.etree.ElementTree as ET # parsing XML
+import json
+from sony_api import CameraAPI
+# pip install opencv-python-headless - cv2 - to capture frams from live view and use JavaScript to update the 'src'attribute of img with live view URL
+import cv2
+import urllib.request
+import numpy as np
+
+# class CameraAPI:
+#     def __init__(self, base_url):
+#         self.base_url = base_url
+
+#     def _make_request(self, method, params):
+#         url = f"{self.base_url}/camera"
+#         payload = {
+#             "method": method,
+#             "params": params,
+#             "id": 1,
+#             "version": "1.0"
+#         }
+#         headers = {
+#             "Content-Type": "application/json"
+#         }
+#         response = requests.post(url, data=json.dumps(payload), headers=headers)
+#         return response
+
+#     def execute_command(self, command, params):
+#         response = self._make_request(command, params)
+#         if response.status_code == 200:
+#             response_data = response.json()
+#             return response_data.get('result', None)
+#         return None
+
+#     def start_liveview(self):
+#         response = self._make_request("startLiveview", [])
+#         if response.status_code == 200:
+#             response_data = response.json()
+#             return response_data.get('result', None)
+#         return None
+
+#     def stop_liveview(self):
+#         response = self._make_request("stopLiveview", [])
+#         if response.status_code == 200:
+#             response_data = response.json()
+#             return response_data.get('result', None)
+#         return None
 
 app = Flask(__name__)
+
+# Add a global variable to store the live view URL
+# liveview_url = None
+# camera_api = None
 
 # Make SSDP dsicovery request on a UDP socket connection
 def discover_camera(): # M-SEARCH (multicast search) request, MX (max wait time for res), ST (search target)
@@ -63,7 +112,6 @@ def describe_camera(url):
         udn_code = root.find(".//root:UDN", namespaces={'root': 'urn:schemas-upnp-org:device-1-0'}).text
         print("UDN:", udn_code)
         api_version = root.find(".//av:X_ScalarWebAPI_Version", namespaces={'av': 'urn:schemas-sony-com:av'}).text
-        print('API Version:', api_version)
         services = []
         for service in root.findall(".//av:X_ScalarWebAPI_Service", namespaces={'av': 'urn:schemas-sony-com:av'}):
             service_type = service.find("av:X_ScalarWebAPI_ServiceType", namespaces={'av': 'urn:schemas-sony-com:av'}).text
@@ -79,11 +127,29 @@ def describe_camera(url):
         for service in services:
             print(f"Service Type: {service['ServiceType']}")
             print(f"Action List URL: {service['ActionListURL']}")
-        endpoint_url = action_list_url + '/camera'
-        return endpoint_url
+        return action_list_url 
     else:
         return print('Unable to retrieve device description.')
 
+def fetch_liveview_data(url):
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        return response.iter_content(chunk_size=1024)
+    return None        
+
+def process_packet(packet):
+    # decode JPEG data
+    try:
+        if packet is not None and len(packet) > 0:
+            np_arr = np.frombuffer(packet, np.uint8)
+            image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            if image is not None:
+                if image.shape[0] > 0 and image.shape[1] > 0:
+                    cv2.imshow('Live View', image)
+                    cv2.waitKey(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
 
 
 @app.route('/')
@@ -93,11 +159,135 @@ def index():
         location_url = handle_discovery_response(response)
         if location_url:
             endpoint_url = describe_camera(location_url)
-            print('Endpoint Url:', endpoint_url)
+            camera_api = CameraAPI(endpoint_url)
+
+            result_rec_mode = camera_api.start_rec_mode()
+            if result_rec_mode is not None and result_rec_mode[0] == 0:
+                print("Started recording mode successfully.")
+                result = camera_api.start_liveview()
+                if result is not None:
+                    liveview_url = result[0]
+                    print("LIVE VIEW URL:", liveview_url)
+                    # OpenCV processing loop
+                    data_stream = fetch_liveview_data(liveview_url)
+                    if data_stream is not None:
+                        for packet in data_stream:
+                            process_packet(packet)
+
+                    cv2.destroyAllWindows()
+                    return "Live view closed."
+                    # return render_template('index.html', liveview_url=liveview_url)
+                    # cap = cv2.VideoCapture(liveview_url)
+                    # ret, frame = cap.read()
+                    # cap.realease()
+                    # if ret:
+                    #     cv2.imwrite('captured_image.jpg', frame)
+                else:
+                    return "Failed to start live view."
+
+            # if camera_api.start_rec_mode() and camera_api.start_liveview():
+            #     return render_template('index.html', liveview_url=liveview_url)
+            # else:
+            #     return "Failed to start recording mode or liveview."
         else:
             return print('Location URL not found.')
     else:
         return print('Device not found.')
+    
+# @app.route('/start_liveview', methods=['POST'])
+# def start_liveview_route():
+#     if camera_api.start_liveview():
+#         return f"Liveview started. URL: {liveview_url}"
+#     else:
+#         return "Failed to start liveview"
+
+# @app.route('/stop_liveview', methods=['POST'])
+# def stop_liveview_route():
+#     if camera_api.stop_liveview():
+#         return "Liveview stopped successfully"
+#     else:
+#         return "Failed to stop liveview"
+
+# @app.route('/capture_picture', methods=['POST'])
+# def capture_picture():
+#     global liveview_url
+
+#     if liveview_url is not None:
+#         cap = cv2.VideoCapture(liveview_url)
+#         ret, frame = cap.read()
+#         cap.release()
+
+#         if ret:
+#             cv2.imwrite('captured_image.jpg', frame)
+#             return "Image captured successfully."
+#         else:
+#             return "Failed to capture image."
+#     else:
+#         return "Live view not available."
+
+# @app.route('/')
+# def index():
+#     response = discover_camera()
+#     if response:
+#         location_url = handle_discovery_response(response)
+#         if location_url:
+#             endpoint_url = describe_camera(location_url)
+#             #intantiate
+#             camera_api = CameraAPI(endpoint_url)
+#             # Start liveview
+#             result = camera_api.start_liveview()
+#             if result is not None:
+#                 liveview_url = result[0]
+#                 print("LIVE VIEW URL:", liveview_url)
+#                 print("Liveview started successfully.")
+#                 return render_template('index.html', liveview_url=liveview_url)
+#             else:
+#                 return "Failed to start liveview."
+#         else:
+#             return print('Location URL not found.')
+#     else:
+#         return print('Device not found.')
+    
+# @app.route('/start_liveview', methods=['POST'])
+# def start_liveview():
+#     global liveview_url, camera_api
+#     result = camera_api.start_liveview()
+
+#     if result is not None:
+#         liveview_url = result[0]
+#         return f"Liveview started. URL: {liveview_url}"
+#     else:
+#         return "Failed to start liveview"
+
+# @app.route('/stop_liveview', methods=['POST'])
+# def stop_liveview():
+#     global liveview_url, camera_api
+#     result = camera_api.stop_liveview()
+
+#     if result is not None and result[0] == 0:
+#         liveview_url = None
+#         return "Liveview stopped successfully"
+#     else:
+#         return "Failed to stop liveview"
+
+# @app.route('/display_liveview')
+# def display_liveview():
+#     global liveview_url
+
+#     if liveview_url is not None:
+#         cap = cv2.VideoCapture(liveview_url)
+#         while True:
+#             ret, frame = cap.read()
+#             if not ret:
+#                 break
+#             cv2.imshow('Live View', frame)
+#             if cv2.waitKey(1) & 0xFF == ord('q'):
+#                 break
+#         cap.release()
+#         cv2.destroyAllWindows()
+#         return "Live view closed."
+#     else:
+#         return "Live view not available."
 
 if __name__ == '__main__':
     app.run(debug=True)
