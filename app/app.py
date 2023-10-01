f866b2710ae433564d5669c670af20ed5f05e219
Jordan <zhao.jordan@hotmail.com>
2023-09-30 19:02:39 -0700

    feat: Successfully queries API endpoint from res received, requests and parses XML

--- app/app.py
+++ app/app.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
-# from flask import Flask, request
-# import requests # pypi.org/project/requests - making HTTP requests
+from flask import Flask
 import sys
-import socket # socket to make connection between nodes of a network
+import socket
+import requests
+import xml.etree.ElementTree as ET # parsing XML
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-SSDP_RECEIVE_TIMEOUT = 10000 # msec
-PACKET_BUFFER_SIZE = 1024
-
-SSDP_PORT = 1900
-SSDP_MX = 1
-SSDP_ADDR = '239.255.255.250'
-SSDP_ST = "urn:schemas-sony-com:service:ScalarWebAPI:1"
-
+# Make SSDP dsicovery request on a UDP socket connection
 def discover_camera(): # M-SEARCH (multicast search) request, MX (max wait time for res), ST (search target)
     discovery_msg = ('M-SEARCH * HTTP/1.1\r\n' +
                     'HOST: 239.255.255.250:1900\r\n' +
                     'MAN: "ssdp:discover"\r\n' +
                     'MX: 3\r\n' +
                     'ST: urn:schemas-sony-com:service:ScalarWebAPI:1\r\n\r\n')
+    #essentially a NOTIFY message to multicast address 239.255.255.250 on port 1900
+
     # ssdp protocol 
-    #ssdp_response = requests.request('M-SEARCH', 'ssdp:discover', data=ssdp_payload) - did not work
-    # instead - let's use sockets to create UDP socket
+    # using sockets to create UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
         #AF_INET = Address Family Internet - family used for IPv4 addresses
         #SOCK_DGRAM = Socket User Datagram Protocol (UDP)
@@ -33,33 +28,76 @@ def discover_camera(): # M-SEARCH (multicast search) request, MX (max wait time 
     # send SSDP request
     sock.sendto(discovery_msg.encode('utf-8'), ('239.255.255.250', 1900) )
     
-    
+    # receive response
     try:
         while True:
             data, addr = sock.recvfrom(1024)
-            print('ADDRESS: ', addr)
-            print('DATA', data)
-            response = data.decode('utf-8')
-            print('DECODED DATA:\n', response)
-            if 'LOCATION' in response:
-                location_url = response.strip().split(' ')[1]
-                print(location_url)   
+            return data.decode('utf-8')  
     except socket.timeout:
-        pass
+        sock.close()
+        return
 
-discover_camera()
+# Handle SSDP discovery response 
+def handle_discovery_response(data):
+    location_line = None
+    for line in data.split('\r\n'):
+        if line.startswith('LOCATION'):
+            location_line = line
+            break
 
-# @app.route('/')
-# def index():
-#     response = discover_camera()
-#     if response:
-#         print('Camera URL found: ', response)
-#     else:
-#         print('Camera not found.')
-#     return
+    if location_line:
+        location_url = location_line.split(': ')[1]
+        print('Location URL:', location_url)
+        return location_url
+    else:
+        return None
 
-# def create_app():
-#    return app
+# Retrieve the device description from location URL XML 
+def describe_camera(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        xml_data = response.text
+        root = ET.fromstring(xml_data)
+        device_name = root.find(".//root:friendlyName", namespaces={'root': 'urn:schemas-upnp-org:device-1-0'}).text
+        print("Device:", device_name)
+        udn_code = root.find(".//root:UDN", namespaces={'root': 'urn:schemas-upnp-org:device-1-0'}).text
+        print("UDN:", udn_code)
+        api_version = root.find(".//av:X_ScalarWebAPI_Version", namespaces={'av': 'urn:schemas-sony-com:av'}).text
+        print('API Version:', api_version)
+        services = []
+        for service in root.findall(".//av:X_ScalarWebAPI_Service", namespaces={'av': 'urn:schemas-sony-com:av'}):
+            service_type = service.find("av:X_ScalarWebAPI_ServiceType", namespaces={'av': 'urn:schemas-sony-com:av'}).text
+            action_list_url = service.find("av:X_ScalarWebAPI_ActionList_URL", namespaces={'av': 'urn:schemas-sony-com:av'}).text
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+            services.append({
+                'ServiceType': service_type,
+                'ActionListURL': action_list_url
+            })
+
+        # Print the extracted information
+        print(f"API Version: {api_version}")
+        for service in services:
+            print(f"Service Type: {service['ServiceType']}")
+            print(f"Action List URL: {service['ActionListURL']}")
+        endpoint_url = action_list_url + '/camera'
+        return endpoint_url
+    else:
+        return print('Unable to retrieve device description.')
+
+
+
+@app.route('/')
+def index():
+    response = discover_camera()
+    if response:
+        location_url = handle_discovery_response(response)
+        if location_url:
+            endpoint_url = describe_camera(location_url)
+            print('Endpoint Url:', endpoint_url)
+        else:
+            return print('Location URL not found.')
+    else:
+        return print('Device not found.')
+
+if __name__ == '__main__':
+    app.run(debug=True)
